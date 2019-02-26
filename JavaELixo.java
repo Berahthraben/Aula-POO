3409d9d6284a3ce03cb769edeb3327e4d63a592e
Berahthraben <nicolasdemantova@hotmail.com>
2019-02-26 20:35:29 -0300

    Create JavaELixo.java

---
+++ JavaELixo.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaelixo;
+    import java.io.*;
+    import java.util.Scanner;
+/**
+ *
+ * @author udesc
+ */
+public class JavaELixo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String entrada = sc.nextLine();
+        
+    }
+    
+}
+public class Fatura {
+    Scanner sc = new Scanner(System.in);
+    String entrada = sc.nextLine();
+    
+}
