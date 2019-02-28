dbd53f953aff5f794d94383d34c977432b62b362
Berahthraben <nicolasdemantova@hotmail.com>
2019-02-27 21:52:40 -0300

    Update JavaELixo.java

--- JavaELixo.java
+++ JavaELixo.java
@@ -17,13 +17,28 @@ public class JavaELixo {
      */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String entrada = sc.nextLine();
-        
+        Fatura fat = new Fatura();
+        String entrada = "a";
+        int entrada2 = 1;
+        double entrada3 = 1;
+        System.out.printf("Digite o nome do produto\n");
+        entrada = sc.nextLine();
+        fat.SetNome(entrada);
+        System.out.printf("Digite a desc do produto\n");
+        entrada = sc.nextLine();
+        fat.SetDesc(entrada);
+        System.out.printf("Digite a qtd. do produto\n");
+        entrada2 = sc.nextInt();
+        fat.SetQtd(entrada2);
+        System.out.printf("Digite o preço do produto\n");
+        entrada3 = sc.nextDouble();
+        fat.SetPre(entrada3);
+        System.out.printf("Printando valores...\n");
+        System.out.printf("Nome do produto: %s\n", fat.GetNome());
+        System.out.printf("Desc do produto: %s\n", fat.GetDesc());
+        System.out.printf("Quantidade do produto: %d\n", fat.GetQtd());
+        System.out.printf("Preço do produto: %.2f\n", fat.GetPre());
+        System.out.printf("Fatura total: %.2f\n", fat.GetFaturaTotal());
     }
     
-}
-public class Fatura {
-    Scanner sc = new Scanner(System.in);
-    String entrada = sc.nextLine();
-    
-}
+} 
