3e3e58a02f3d111d1a98eeae8c04d07b0b1aedac
Berahthraben <nicolasdemantova@hotmail.com>
2019-03-11 15:51:00 -0300

    Create JoguinhoAula2PSJavasucks.java

---
+++ Aula2/EX3/JoguinhoAula2PSJavasucks.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package joguinhoaula2.ps.javasucks;
+    import java.util.*;
+    import java.io.*;
+/**
+ *
+ * @author udesc
+ */
+public class JoguinhoAula2PSJavasucks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nome;
+        int str;
+        int con;
+        int dex;
+        int agi;
+        Personagem pg1 = new Personagem("siccardi", 10, 13, 15, 20);
+        Personagem pg2 = new Personagem("Jovivel", 69, 24, 63, 20);
+        Personagem pg3 = new Personagem("Nicol Bolas", 59, 23, 45 ,23);
+        pg4.GetQtd();
+    }
+    
+}
